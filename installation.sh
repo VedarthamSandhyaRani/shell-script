664afd497d8698cdaee051f7343837e5ae0b4850
VedarthamSandhyaRani <sandhyaranivedartham12@gmail.com>
2025-05-25 11:41:55 +0530

    JLK

--- installation.sh
+++ installation.sh
@@ -5,8 +5,11 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
 echo "Please run this script with root access"
+exit 1
 else
 echo "you are super user"
 fi
 
 dnf install sql -y
+
+echo "is script proceeding?"
