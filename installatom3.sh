14902f78814d8b549a27d26325bad52acef46292
VedarthamSandhyaRani <sandhyaranivedartham12@gmail.com>
2025-05-26 13:22:37 +0530

    GLtyyglk

--- installatom3.sh
+++ installatom3.sh
@@ -5,6 +5,7 @@ USERID=$(id -u)
 validate()
 {
     echo "Exit status:$1"
+    echo "Exit status:$2"
 }
 
 if [ $USERID -ne 0 ]
@@ -16,5 +17,7 @@ else
 fi
 
 sudo dnf install -y https://dev.mysql.com/get/mysql80-community-release-el8-1.noarch.rpm
-sudo dnf install mariadb-server -y
 Validate $? "installating my sql" 
+sudo apt update
+sudo apt install -y openjdk-17-jdk
+Validate $? "installating JDK" 
