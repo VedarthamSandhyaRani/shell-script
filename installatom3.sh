153f9868bb5a8ae752ffe7a6f3728ad4501624f6
VedarthamSandhyaRani <sandhyaranivedartham12@gmail.com>
2025-05-26 13:18:04 +0530

    GLtyy

--- installatom3.sh
+++ installatom3.sh
@@ -15,5 +15,5 @@ else
   echo "you are super user"
 fi
 
-sudo dnf install -y mariadb-server
+sudo dnf install -y https://dev.mysql.com/get/mysql80-community-release-el8-1.noarch.rpm
 Validate $? "installating my sql" 
